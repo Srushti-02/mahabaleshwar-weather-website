c8337a8e30bd28409e93d385ddd5afb986019781
Srushti-02 <srushta.272002@gmail.com>
2023-11-30 22:47:57 +0530

    css changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
+
 async function getWeather(){
-    
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?units=metric&q=Mahabaleshwar,in&APPID=5e22af05e319187dc6b2e0df5c99ff3d");
     var data = await response.json();
 
@@ -9,6 +9,19 @@ async function getWeather(){
     document.querySelector(".hdt").innerHTML = data.main.humidity+" %";
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+" °C";
     document.querySelector(".ws").innerHTML = data.wind.speed+" km/hr";
+
+    if(data.weather[0].main == 'Clouds'){
+        document.querySelector(".weather-icon").src = 'cloudy_sunny.png';
+    }
+    else if(data.weather[0].main == 'Clear'){
+        document.querySelector(".weather-icon").src = 'sun.png';
+    }
+    else if(data.weather[0].main == 'Rain'){
+        document.querySelector(".weather-icon").src = 'rain.png';
+    }
+    else{
+        document.querySelector(".weather-icon").src = 'cloudy_sunny.png';
+    }
 }
 
 getWeather();
